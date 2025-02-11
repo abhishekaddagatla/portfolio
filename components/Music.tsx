0b9cea63eb687e0bf0182ae7c351d721c218b595
Abhishek Addagatla <45775590+abhirawr@users.noreply.github.com>
2025-02-11 15:15:22 -0600

    added soundcloud link to music page

--- components/Music.tsx
+++ components/Music.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Music = () => {
   return (
-    <div className="h-full w-full flex justify-center">
+    <div className="h-full w-full flex flex-col items-center">
       <h1 className="mt-28 text-4xl">
         🚧 this page is currently under construction! check back later! 🚧
       </h1>
+      <p className="mt-12">until then, here is my <a className=" text-blue-500 font-bold" target="_blank" href="https://soundcloud.com/explct69">soundcloud</a></p>
     </div>
   );
 };
